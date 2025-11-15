8ae7b078b080b98653e19929c8082463126c022f
GigMate 6 Actual <gigmate10@gmail.com>
2025-11-15 13:05:32 -0600

    Updated gigmate-logo.svg

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -20,7 +20,7 @@ export default function Header({ onLogoClick }: HeaderProps) {
             className="flex items-center hover:opacity-80 transition-opacity"
           >
             <img
-              src="/gigmate-pick.svg"
+              src="/gigmate-pick.png"
               alt="GigMate Logo"
               className="h-16 w-16"
             />
